7d6d5e9b9540ce234fc3ac69e485eee80ffbc37a
haseoungwon-creator <230819420+haseoungwon-creator@users.noreply.github.com>
2026-05-18 21:43:49 +0900

    (2026.5.18) 수출용 패키지

---
+++ Program/Assets/Class 3th(User Interface)/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void Continue()
+    {
+        Debug.Log("start");
+    }
+
+    public void StartNewGame()
+    {
+        Debug.Log("Start New Game");
+    }
+
+    public void OpenOption()
+    {
+        Debug.Log("Option");
+    }
+    public void Quit()
+    {
+        Debug.Log("Quit");
+    }
+
+}
