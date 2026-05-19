d8286bf36f3df283563503c9b608705b47a37aa4
haseoungwon-creator <230819420+haseoungwon-creator@users.noreply.github.com>
2026-05-19 21:44:01 +0900

    (2026.5.19) 애니메이션 시스템

---
+++ Program/Assets/Class 4th(Animation)/Scripts/Axe.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Axe : MonoBehaviour
+{
+
+    public void Attack(int count)
+    {
+        Debug.Log("Attack");
+        
+        if (count > 10)
+        {
+            Debug.Log("Critical");
+        }
+    }
+
+    public void Defend(int defensive_power)
+    {
+        Debug.Log("Defend");
+    }
+
+    public void Throw(int throw_damage)
+    {
+        Debug.Log("Throw");
+    }
+}
