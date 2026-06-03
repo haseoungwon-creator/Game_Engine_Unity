d7c18347b5ed35e18aa48ec55a8eb83251098a0a
haseoungwon-creator <230819420+haseoungwon-creator@users.noreply.github.com>
2026-06-03 21:44:26 +0900

    (2026.6.3) 코루틴

--- Program/Assets/Class 9th(Coroutine)/Scripts/Create Manager.cs
+++ Program/Assets/Class 9th(Coroutine)/Scripts/Create Manager.cs
@@ -1,18 +1,35 @@
 using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+
 
 public class CreateManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] GameObject elemental;
+    [SerializeField] List<GameObject> objList = new List<GameObject>();
+    [SerializeField] float timer = 0f;
+    [SerializeField] int count = 0;
     void Start()
     {
-        for(int i = 0; i < 5; i++)
+        
+        for (int i = 0; i < 5; i++)
         {
-            GameObject tree= Instantiate(elemental, transform);
-            tree.transform.position = new Vector3 (-3 + (1.5f * i), 1,1);
-            
+            GameObject tree = Instantiate(elemental, transform);
+            objList.Add(tree);
+            tree.SetActive(false);
+            tree.transform.position = new Vector3(-3 + (1.5f * i), 1, 1);
+
         }
+        StartCoroutine(Coroutine());
+    }
+
+    IEnumerator Coroutine()
+    {
+        Debug.Log("Start");
+        yield return new WaitForSeconds(5.0f);
+        Debug.Log("Stop_");
     }
 
 }
