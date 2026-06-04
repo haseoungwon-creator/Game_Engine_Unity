dacaed252b085a707c06a312b00deabd0de2b0dc
haseoungwon-creator <230819420+haseoungwon-creator@users.noreply.github.com>
2026-06-04 21:44:00 +0900

    (2026.6.4) 광원 시스템

---
+++ Program/Assets/Class 10th(Sound System)/Scripts/Interaction.cs
@@ -0,0 +1,20 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+    [SerializeField] Ray ray;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        Gizmos.DrawRay(ray.origin, ray.direction*100);
+    }
+}
--- Program/Assets/Class 9th(Coroutine)/Scripts/Create Manager.cs
+++ Program/Assets/Class 9th(Coroutine)/Scripts/Create Manager.cs
@@ -1,6 +1,8 @@
+using JetBrains.Annotations;
 using NUnit.Framework;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
 
 
@@ -9,8 +11,9 @@ public class CreateManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] GameObject elemental;
     [SerializeField] List<GameObject> objList = new List<GameObject>();
-    [SerializeField] float timer = 0f;
     [SerializeField] int count = 0;
+    [SerializeField] int last;
+    WaitForSeconds waitforseconds = new WaitForSeconds(5f);
     void Start()
     {
         
@@ -20,16 +23,26 @@ public class CreateManager : MonoBehaviour
             objList.Add(tree);
             tree.SetActive(false);
             tree.transform.position = new Vector3(-3 + (1.5f * i), 1, 1);
-
         }
+        last = objList.Count;
+        
         StartCoroutine(Coroutine());
     }
 
     IEnumerator Coroutine()
     {
-        Debug.Log("Start");
-        yield return new WaitForSeconds(5.0f);
-        Debug.Log("Stop_");
+        //for (count = 0; count < objList.Count; count++)
+        //{
+        //    objList[count].SetActive(true);
+        //    yield return new WaitForSeconds(5.0f);
+        //}
+
+        while (count < objList.Count)
+        {
+            objList[count++].SetActive(true);
+            yield return waitforseconds;
+        }
+
     }
 
 }
